b416505458eea5e3ddac9e515ddb7b7ce8d98753
김기휘 <kimgihwi@gmail.com>
2020-09-04 11:19:20 +0900

    Create cam_test.py

---
+++ cam_test.py
@@ -0,0 +1,16 @@
+import cv2
+import matplotlib.pyplot as plt
+
+cam_cap = cv2.VideoCapture(0)
+
+
+while True:
+    _, frame = cam_cap.read()
+
+    cv2.imshow('self cam', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam_cap.release()
+cv2.destroyAllWindows()
